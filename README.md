8f81e8af4c12c8019017ab7b5ac0e1881c6f1946
David Keller <mrcodeswildride@gmail.com>
2020-08-26 12:36:35 +0000

    es6 update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # swap
 
-This is a swap illustration written in HTML, CSS, and JavaScript.
+Swap illustration written in HTML, CSS, and JavaScript.
 
 See this illustration at [swap.mrcodeswildride.com](https://swap.mrcodeswildride.com/).
 See other illustrations at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
--- script.js
+++ script.js
@@ -1,61 +1,64 @@
-var cup1 = document.getElementById("cup1");
-var cup2 = document.getElementById("cup2");
-var cup3 = document.getElementById("cup3");
-var message = document.getElementById("message");
+let cup1 = document.getElementById(`cup1`)
+let cup2 = document.getElementById(`cup2`)
+let cup3 = document.getElementById(`cup3`)
+let messageParagraph = document.getElementById(`messageParagraph`)
 
-var firstSelectedCup = null;
+let selectedCup
 
-cup1.addEventListener("click", select);
-cup2.addEventListener("click", select);
-cup3.addEventListener("click", select);
+cup1.addEventListener(`click`, clickCup)
+cup2.addEventListener(`click`, clickCup)
+cup3.addEventListener(`click`, clickCup)
 
-function select() {
-    if (firstSelectedCup == null) {
-        // select cup
-        this.classList.add("selected");
-        firstSelectedCup = this;
-    }
-    else {
-        if (firstSelectedCup != this) {
-            // get contents of both cups
-            var firstSelectedCupContents = firstSelectedCup.firstElementChild.className;
-            var secondSelectedCupContents = this.firstElementChild.className;
+function clickCup() {
+  if (selectedCup == null) {
+    // select cup
+    selectedCup = this
+    selectedCup.classList.add(`selected`)
+  }
+  else {
+    if (selectedCup != this) {
+      // get contents of selected cups
+      let firstCupContents = selectedCup.firstElementChild.className
+      let secondCupContents = this.firstElementChild.className
 
-            if (firstSelectedCupContents != "empty") {
-                // empty the first selected cup
-                firstSelectedCup.firstElementChild.className = "empty";
+      if (firstCupContents != `empty`) {
+        // empty first cup
+        selectedCup.firstElementChild.className = `empty`
 
-                // determine what should be in the second selected cup
-                if (secondSelectedCupContents == "empty") {
-                    // second selected cup will have whatever was in first selected cup
-                    this.firstElementChild.className = firstSelectedCupContents;
+        // determine what should be in second cup
+        if (secondCupContents == `empty`) {
+          // second cup will have whatever was in first cup
+          this.firstElementChild.className = firstCupContents
 
-                    if (isSolved()) {
-                        // show message and disable cup selection
-                        message.innerHTML = "Good job!";
-                        cup1.removeEventListener("click", select);
-                        cup2.removeEventListener("click", select);
-                        cup3.removeEventListener("click", select);
-                    }
-                }
-                else {
-                    // second selected cup will have mixed juice
-                    this.firstElementChild.className = "mixed";
-                }
-            }
+          // check if user solved illustration
+          checkSolved()
         }
-
-        // start over
-        firstSelectedCup.classList.remove("selected");
-        firstSelectedCup = null;
+        else {
+          // second cup will have mixed juice
+          this.firstElementChild.className = `mixed`
+        }
+      }
     }
+
+    // start over
+    selectedCup.classList.remove(`selected`)
+    selectedCup = null
+  }
 }
 
-function isSolved() {
-    // get contents of cup 1 and cup 2
-    var cup1Contents = cup1.firstElementChild.className;
-    var cup2Contents = cup2.firstElementChild.className;
+function checkSolved() {
+  // get contents of cup 1 and cup 2
+  let cup1Contents = cup1.firstElementChild.className
+  let cup2Contents = cup2.firstElementChild.className
 
-    // check if cup 1 has grape juice and cup 2 has orange juice
-    return cup1Contents == "grape" && cup2Contents == "orange";
+  // check if cup 1 has grape juice and cup 2 has orange juice
+  if (cup1Contents == `grape` && cup2Contents == `orange`) {
+    // show message
+    messageParagraph.innerHTML = `Good job!`
+
+    // disable cup selection
+    cup1.removeEventListener(`click`, clickCup)
+    cup2.removeEventListener(`click`, clickCup)
+    cup3.removeEventListener(`click`, clickCup)
+  }
 }
